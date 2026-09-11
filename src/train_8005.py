9c669dc45148d227fc7847806d05fa146d1643c1
sabasajid09 <saba.sajid09@gmail.com>
2026-09-11 22:15:53 +0500

    Implement MinMaxScaler feature tuning in Branch tuning

--- src/train_8005.py
+++ src/train_8005.py
@@ -2,6 +2,7 @@ print("Loading dataset for Student ID: Saba_8005")
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import MinMaxScaler
 import joblib
 
 # Student ID
@@ -24,6 +25,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     random_state=42
 )
+
+### changes start
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+### changes end
 
 # 4. Create and train the machine learning model
 model = RandomForestRegressor(
